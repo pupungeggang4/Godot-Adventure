9e028230983e11bcdab8d07e654fb31ad948bf44
pupungeggang2 <kyongkyong2892@gmail.com>
2025-11-27 16:01:40 +0900

    Temp

--- game/script/thing/field_player.gd
+++ game/script/thing/field_player.gd
@@ -9,4 +9,4 @@ func _physics_process(delta: float) -> void:
     if GVar.state == '':
         var direction = Input.get_vector("left", "right", "up", "down")
         velocity = direction * speed
-        move_and_collide(velocity * delta)
+        move_and_slide()
