65d3ee91edd68f1fa9163ce11c0593817b784f56
pupungeggang2 <kyongkyong2892@gmail.com>
2025-12-04 14:39:13 +0900

    Title Screen

--- game/script/scene/title.gd
+++ game/script/scene/title.gd
@@ -13,3 +13,13 @@ func _on_color_rect_gui_input(event: InputEvent) -> void:
     if event is InputEventMouseButton:
         if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
             get_tree().change_scene_to_file('res://scene/field.tscn')
+
+func _on_button_collection_gui_input(event: InputEvent) -> void:
+    if event is InputEventMouseButton:
+        if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+            get_tree().change_scene_to_file('res://scene/collection.tscn')
+
+func _on_button_quit_gui_input(event: InputEvent) -> void:
+    if event is InputEventMouseButton:
+        if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+            get_tree().quit()
